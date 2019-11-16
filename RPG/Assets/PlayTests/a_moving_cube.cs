e65390c111c6e7f89a4665f22370212369e2aa85
Daniel Jaffe <flarros@gmail.com>
2019-11-15 16:12:17 -0800

    Project creation
    Also some experimentation with unit tests

---
+++ RPG/Assets/PlayTests/a_moving_cube.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class a_moving_cube
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void NewTestScriptSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator moving_forward_changes_position()
+        {
+            // ARRANGE
+            var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.transform.position = Vector3.zero;
+            
+            // ACT
+            for (int i = 0; i < 10; i++)
+            {
+                cube.transform.position += Vector3.forward;
+                yield return null; // new WaitForSeconds(0.5f);
+                
+                // ASSERT
+                Assert.AreEqual(i + 1,cube.transform.position.z);
+            }
+            
+            // ASSERT
+            
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
---
+++ RPG/Assets/Tests/NewTestScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Boo.Lang.Environments;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class NewTestScript
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void NewTestScriptSimplePasses()
+        {
+            List<string> someStrings = new List<string>();
+            someStrings.Add("My string!");
+            someStrings.Add("My second string!");
+            someStrings.RemoveAt(0);
+            Assert.AreEqual(1, someStrings.Count);
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator NewTestScriptWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
